b5497fcd385b43998f1c1ff982545695e8ae72ca
Ismael Abed <Abed.coism20@zaragoza.salesianos.edu>
2024-01-26 11:57:52 +0100

    Modificaciones 26 de Enero de 2024. Son meras pruebas para implementar el comando /JOIN

--- echo-client.py
+++ echo-client.py
@@ -3,36 +3,44 @@
 import socket
 import time
 
-HOST = "127.0.0.1"
+# Recogemos el nombre del Host para obtener su IP
+HOSTIP = "127.0.0.1"
+PORT = 65432
+
 PORT = 65432
 
 TIEMPO_ESPERA = 60  # segundos
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    
+    s.connect((HOSTIP, PORT))
+
     # Establecer un tiempo de espera para el cliente
     s.settimeout(TIEMPO_ESPERA)
-    
+
     while True:
         try:
             # Solicitar al usuario que ingrese un mensaje
-            message = input("Ingrese un mensaje (o presione Enter para salir): ")
-            
+            message = input(
+                "Ingrese un mensaje (o presione Enter para salir): ")
+
             # Salir del bucle si no se ingresa ningún mensaje
             if not message:
                 break
-            
-            # Convertir el mensaje a bytes y enviarlo al servidor
-            s.sendall(message.encode())
-            
-            # Recibir la respuesta del servidor
-            data = s.recv(1024)
-            
-            # Decodificar y mostrar la respuesta recibida del servidor
-            response_from_server = data.decode()
-            print(f"Mensaje recibido del servidor: {response_from_server}")
-        
+
+            print(
+                f"Mensaje recibido del servidor: {comprobarRespuestaServidor()}")
+
         except socket.timeout:
             print("Tiempo de espera alcanzado. Cerrando conexión.")
             break
+
+
+def comprobarRespuestaServidor():
+    # Recibir la respuesta del servidor
+    data = s.recv(1024)
+    # Decodificar y guardar el mensaje del servidor
+    mensaje = data.decode()
+
+
+def join_client(mensaje):
+    mensaje = mensaje.split(" ")
--- echo-server.py
+++ echo-server.py
@@ -8,17 +8,17 @@ PORT = 65432
 TIEMPO_ESPERA = 60  # segundos
 
 # Inicializar la variable para almacenar el mensaje del cliente
-input_client = ""  
+input_client = ""
 
-#Lista de strings con las opciones que se pueden usar como comandos
-command_list = ["LIST", "CREATE", "CONNECT", "JOIN", "MSG"]
+# Lista de strings con las opciones que se pueden usar como comandos
+command_list = ["/LIST", "/CREATE", "/CONNECT", "/JOIN", "/MSG"]
 
-#Diccionarios
+# Diccionarios
 channel = {
     'canal1': '[]',
     'canal2': '[ismael, jose]',
     'canal3': '[ismael, juan]'
-    }
+}
 # Creacion de canales usando diccionarios
 users = {
     'ismael': '192.168.1.2',
@@ -26,7 +26,8 @@ users = {
     'juan': '192.168.1.4'
 }
 
-user=("ismael", "192.168.1.2")
+user = ("ismael", "192.168.1.2")
+
 
 def establecerConexion():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -35,10 +36,10 @@ def establecerConexion():
         conn, addr = s.accept()
         with conn:
             print(f"Conectado por {addr}")
-            
+
             # Establecer un tiempo de espera para el servidor
             conn.settimeout(TIEMPO_ESPERA)
-            
+
             while True:
                 try:
                     data = conn.recv(1024)
@@ -48,51 +49,64 @@ def establecerConexion():
 
                     # Decodificar y guardar el mensaje del cliente
                     input_client = data.decode()
-                    
+
                     # Modificar el mensaje a enviar de vuelta al cliente
                     response_to_client = f"Mensaje desde el servidor: {input_client}"
 
                     # Enviar de vuelta el mensaje modificado al cliente
                     conn.sendall(response_to_client.encode())
-                    admitirComandos(input_client)
+                    admitirComandos(input_client, conn)
 
                 except socket.timeout:
                     print("Tiempo de espera alcanzado. Cerrando conexión.")
                     break
-                
+
+
 """Metodo admitir comandos, 
 Ve que el comando empiece por / o // o + y luego compruebe que la cadena de texto  sea en mayusculas. 
 O que sea en minusculas o mayusculas el metodo establezca todo a mayusculas y sean palabras reservadas
 Comprobar si la variable comienza con "/" y el resto está en mayúsculas y en la lista"""
-def admitirComandos(input_client):
-    if input_client.startswith("/"):
-        print("El input empieza por /")
+
+
+def admitirComandos(input_client, conn):
+    if input_client.upper() in command_list:
+        print("El input es un comando valido")
+        if input_client.equals("/JOIN"):
+            join_channel(user, conn)
+
         if input_client[1:].isupper() and input_client[1:] in command_list:
             print("El input es un comando valido")
-            #Falta llamar a la funcion correspondiente, por ahora no está implementado porque no sabemos como se van a llamar las funciones
+            # Falta llamar a la funcion correspondiente, por ahora no está implementado porque no sabemos como se van a llamar las funciones
         else:
             print("El comando no existe o no está escrito correctamente. Recuerda que los comandos son en mayusculas y empiezan por /")
     else:
         print("El input no incluye un comando (no comienza por /)")
 
-#Llamada al metodo establecerConexion        
-establecerConexion() 
 
+# ESTA BIEN
+def join_channel(user, conn):
+    channel = list(channel.keys())  # Lista de canales existentes
+    message = print("/JOIN Canales existentes: " + channel)
+    # Enviar de vuelta el mensaje modificado al cliente
+    conn.sendall(message.encode())
 
-
-def join_channel(command, user):
-    if command == "/JOIN":
-        channel = list(channel.keys())  # Lista de canales existentes
-        print("Canales existentes:", channel)
-        channel_name = input(
-            "Introduce el nombre del canal al que quieres unirte: ")
-        # Añade el usuario al canal indicado
-        if channel_name in channel:
-            channel[channel_name].append(user)
-            print(f"Usuario '{user}' añadido al canal '{channel_name}'.")
-        else:
-            print(f"El canal '{channel_name}' no existe.")
+    # Recibir la respuesta del cliente y decodificarla, guardandola en la variable data
+    # será el nombre del canal al que se quiere unir
+    data = conn.recv(1024)
+    # Decodificar y guardar el mensaje del cliente
+    input_client = data.decode()
+    # Comprobar si el canal existe
+    if input_client in channel:
+        # Si existe, añadir al usuario a la lista de usuarios del canal
+        channel[input_client].append(user)
+        # Enviar de vuelta el mensaje modificado al cliente
+        conn.sendall(message.encode())
     else:
-        print(f"El comando no es válido.")
+        # Si no existe, enviar mensaje de error
+        message = print("El canal no existe")
+        # Enviar de vuelta el mensaje modificado al cliente
+        conn.sendall(message.encode())
 
-join_channel(input_client, user)
+
+# Llamada al metodo establecerConexion
+establecerConexion()
